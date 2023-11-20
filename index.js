d82fedd12e062eb5591fd809a59f57be4fbbbe02
Muhamad Nur Syami <nursami385@gmail.com>
2023-11-20 20:36:25 +0700

    pindah commiit

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const mongoose = require("mongoose");
 const cloudinary = require("cloudinary").v2;
 const multer = require("multer");
-const Videos = require("./models/video");
+const Video = require("./models/video");
 const Buku = require("./models/buku");
 require("dotenv").config();
 const cors = require("cors");
@@ -29,13 +29,13 @@ app.use(cors());
 
 app.post("/upload", upload.single("file"), async (req, res) => {
   try {
-    const result = await cloudinary.uploader
+    await cloudinary.uploader
       .upload_stream({ resource_type: "auto" }, (error, result) => {
         if (error) {
           return res.status(500).json({ error: "Cloudinary upload failed" });
         }
         if (req.body.type === "video") {
-          let video = new Videos({
+          let video = new Video({
             title: req.body.title,
             description: req.body.description,
             author: req.body.author,
@@ -49,22 +49,23 @@ app.post("/upload", upload.single("file"), async (req, res) => {
           });
           video.save();
           res.json(video);
-        } else if (req.body.type === "buku") {
-          let buku = new Buku({
-            title: req.body.title,
-            description: req.body.description,
-            author: req.body.author,
-            tahun_terbit: req.body.tahun_terbit,
-            rating: req.body.rating,
-            star: req.body.star,
-            img_url: req.body.img_url,
-            category: req.body.category,
-            book_url: result.secure_url,
-            download_url: result.secure_url,
-          });
-          buku.save();
-          res.json(buku);
         }
+        // else if (req.body.type === "buku") {
+        //   let buku = new Buku({
+        //     title: req.body.title,
+        //     description: req.body.description,
+        //     author: req.body.author,
+        //     tahun_terbit: req.body.tahun_terbit,
+        //     rating: req.body.rating,
+        //     star: req.body.star,
+        //     img_url: req.body.img_url,
+        //     category: req.body.category,
+        //     book_url: result.secure_url,
+        //     download_url: result.secure_url,
+        //   });
+        //   buku.save();
+        //   res.json(buku);
+        // }
       })
       .end(req.file.buffer);
     // res.json(result);
@@ -73,22 +74,5 @@ app.post("/upload", upload.single("file"), async (req, res) => {
     res.status(500).json({ error: "Internal Server Error" });
   }
 });
-
-// app.post("/upload", upload.single("file"), (req, res) => {
-//   const file = req.file;
-
-//   // Upload file to Cloudinary
-//   cloudinary.uploader
-//     .upload_stream({ resource_type: "auto" }, (error, result) => {
-//       if (error) {
-//         return res.status(500).json({ error: "Cloudinary upload failed" });
-//       }
-
-//       // You can do something with the Cloudinary result, e.g., save the URL to a database
-//       const publicUrl = result.secure_url;
-//       res.json({ url: publicUrl });
-//     })
-//     .end(file.buffer);
-// });
 
 app.listen(5000, () => console.log("Server Running"));
