e553af26b0bcc1402a001a9e61424b81d05a3410
Muhamad Nur Syami <nursami385@gmail.com>
2023-11-20 22:35:42 +0700

    bisa fitur video dan file

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ const app = express();
 const mongoose = require("mongoose");
 const cloudinary = require("cloudinary").v2;
 const multer = require("multer");
-// const Video = require("./models/video");
-// const Buku = require("./models/buku");
+const Video = require("./models/video");
+const Buku = require("./models/buku");
 require("dotenv").config();
 const cors = require("cors");
 // Connect DB
@@ -49,9 +49,39 @@ app.post("/upload", upload.single("file"), async (req, res) => {
             details: error.message,
           });
         }
-
+        if (req.body.type === "video") {
+          let video = new Video({
+            title: req.body.title,
+            description: req.body.description,
+            author: req.body.author,
+            likes: req.body.likes,
+            rating: req.body.rating,
+            category: req.body.category,
+            tanggal_upload: req.body.tanggal_upload,
+            url_thumbnail: req.body.url_thumbnail,
+            url_video: req.body.url_video,
+            url_unduh: result.secure_url,
+          });
+          video.save();
+          res.json(video);
+        } else if (req.body.type === "buku") {
+          let buku = new Buku({
+            title: req.body.title,
+            description: req.body.description,
+            author: req.body.author,
+            tahun_terbit: req.body.tahun_terbit,
+            rating: req.body.rating,
+            star: req.body.star,
+            img_url: req.body.img_url,
+            category: req.body.category,
+            book_url: result.secure_url,
+            download_url: result.secure_url,
+          });
+          buku.save();
+          res.json(buku);
+        }
         // Tanggapan dengan hasil upload dari Cloudinary
-        res.json(result);
+        // res.json(result);
       }
     );
 
