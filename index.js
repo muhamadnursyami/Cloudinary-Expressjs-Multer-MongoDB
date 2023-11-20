a3ec21cba36471dc3d3a2a5f3874c9e906b9fa29
Muhamad Nur Syami <nursami385@gmail.com>
2023-11-20 22:26:55 +0700

    perbaharui code

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ const app = express();
 const mongoose = require("mongoose");
 const cloudinary = require("cloudinary").v2;
 const multer = require("multer");
-const Video = require("./models/video");
-const Buku = require("./models/buku");
+// const Video = require("./models/video");
+// const Buku = require("./models/buku");
 require("dotenv").config();
 const cors = require("cors");
 // Connect DB
@@ -12,9 +12,6 @@ mongoose
   .connect(process.env.MONGOO_URI)
   .then(() => console.log("Mongo DB is Connected"))
   .catch((error) => console.error("Error connecting to MongoDB:", error));
-// multer
-const storage = multer.memoryStorage();
-const upload = multer({ storage: storage });
 
 // cloudinary
 cloudinary.config({
@@ -23,52 +20,43 @@ cloudinary.config({
   api_secret: process.env.CLOUDDINARY_API_SECRET,
 });
 
+// Konfigurasi Multer untuk menyimpan di dalam memori
+const storage = multer.memoryStorage();
+const upload = multer({
+  storage: storage,
+  limits: { fileSize: 100 * 1024 * 1024 }, // Batas ukuran file (100 MB)
+});
 // Middlerware
 app.use(express.json());
 app.use(cors());
 
+// Endpoint untuk mengunggah file/video
 app.post("/upload", upload.single("file"), async (req, res) => {
   try {
-    await cloudinary.uploader
-      .upload_stream({ resource_type: "auto" }, (error, result) => {
+    // Periksa apakah file/video diunggah
+    if (!req.file) {
+      return res.status(400).json({ error: "No file or video uploaded" });
+    }
+
+    // Ubah buffer menjadi readable stream
+    const stream = cloudinary.uploader.upload_stream(
+      { resource_type: "auto", timeout: 120000 }, // Batas waktu unggah (120 detik)
+      (error, result) => {
+        // Handle Cloudinary upload errors
         if (error) {
-          return res.status(500).json({ error: "Cloudinary upload failed" });
-        }
-        if (req.body.type === "video") {
-          let video = new Video({
-            title: req.body.title,
-            description: req.body.description,
-            author: req.body.author,
-            likes: req.body.likes,
-            rating: req.body.rating,
-            category: req.body.category,
-            tanggal_upload: req.body.tanggal_upload,
-            url_thumbnail: req.body.url_thumbnail,
-            url_video: req.body.url_video,
-            url_unduh: result.secure_url,
+          return res.status(500).json({
+            error: "Cloudinary upload failed",
+            details: error.message,
           });
-          video.save();
-          res.json(video);
         }
-        // else if (req.body.type === "buku") {
-        //   let buku = new Buku({
-        //     title: req.body.title,
-        //     description: req.body.description,
-        //     author: req.body.author,
-        //     tahun_terbit: req.body.tahun_terbit,
-        //     rating: req.body.rating,
-        //     star: req.body.star,
-        //     img_url: req.body.img_url,
-        //     category: req.body.category,
-        //     book_url: result.secure_url,
-        //     download_url: result.secure_url,
-        //   });
-        //   buku.save();
-        //   res.json(buku);
-        // }
-      })
-      .end(req.file.buffer);
-    // res.json(result);
+
+        // Tanggapan dengan hasil upload dari Cloudinary
+        res.json(result);
+      }
+    );
+
+    // Kirim buffer sebagai readable stream ke Cloudinary
+    stream.end(req.file.buffer);
   } catch (error) {
     console.error(error);
     res.status(500).json({ error: "Internal Server Error" });
